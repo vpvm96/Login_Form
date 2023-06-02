5ae3aa9499c252621f4142b30d41c57e0e9849f9
vpvm96 <vpvm96@gmail.com>
2023-06-02 17:14:16 +0900

    feat: 쿠키를 통한 로그인 여부 확인하여 Login & Logout 버튼 보여주기

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,55 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <div>Home</div>;
+  const [auth, setAuth] = useState(false);
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:8081").then((res) => {
+      if (res.data.Status === "Success") {
+        setAuth(true);
+        setName(res.data.name);
+      } else {
+        setAuth(false);
+        setMessage(res.data.Message);
+      }
+    });
+  }, []);
+
+  const handleLogout = () => {
+    axios
+      .get("http://localhost:8081/logout")
+      .then((res) => {
+        if (res.data.Status === "Success") {
+          window.reload(true);
+        } else {
+          alert("Error");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="container mt-4">
+      {auth ? (
+        <div>
+          <h3>You are Authorized {name}</h3>
+          <button className="btn btn-danger" onClick={handleLogout}>
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h3>{message}</h3>
+          <h3>Login Now</h3>
+          <Link to="/login" className="btn btn-primary">
+            Login
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 }
